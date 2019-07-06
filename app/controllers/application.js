7e22f730ed520cbfeb667e0c85f743742d45c4cd
saikota <sai.kota@hotmail.com>
2019-07-06 12:05:04 -0700

    working chefs

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,28 @@
 import Controller from '@ember/controller';
-
+import Ember from 'ember';
 export default Controller.extend({
-  name:'Sai'
+  name:'Sai',
+  newItem:"",
+  menuAvailable:Ember.computed.filterBy('model','isAvailable',true),
+  menuLength:Ember.computed.alias('model.length'),
+  actions:{
+    makeUnavailable(food){
+       Ember.set(food, 'isAvailable', false);
+      food.save();
+    },
+    makeAvailable(food){
+       Ember.set(food, 'isAvailable', true);
+      food.save();
+    },
+    saveItem(){
+      this.store.createRecord('food',{
+        name:this.get('newItem'),
+        isAvailable:false
+      }).save();
+      this.set('newItem',"");
+    },
+    destroy(food){
+      food.destroyRecord();
+    }
+  }
 });

23c0dd6af047b15dcd4e841ae11c8da67ca41ec6
saikota <sai.kota@hotmail.com>
2019-07-06 12:54:52 -0700

    challenge 1

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,24 +5,40 @@ export default Controller.extend({
   newItem:"",
   menuAvailable:Ember.computed.filterBy('model','isAvailable',true),
   menuLength:Ember.computed.alias('model.length'),
+  orderSum:Ember.computed.mapBy('model','numberOfOrders'),
+  numOrdersCount: Ember.computed.sum('orderSum'),
+  numOrders:'',
   actions:{
     makeUnavailable(food){
-       Ember.set(food, 'isAvailable', false);
+      Ember.set(food, 'isAvailable', false);
       food.save();
     },
     makeAvailable(food){
-       Ember.set(food, 'isAvailable', true);
+      Ember.set(food, 'isAvailable', true);
       food.save();
     },
     saveItem(){
       this.store.createRecord('food',{
         name:this.get('newItem'),
-        isAvailable:false
+        isAvailable:false,
+        numberOfOrders:this.get('numOrders')
       }).save();
       this.set('newItem',"");
+      this.set('numOrders',"")
     },
     destroy(food){
       food.destroyRecord();
-    }
+    },
+    increaseOrder(food){
+
+        Ember.set(food,'numberOfOrders', Ember.get(food,'numberOfOrders')+1);
+        food.save();
+    },
+    decreaseOrder(food){
+     if(Ember.get(food,'numberOfOrders')>1){
+       Ember.set(food,'numberOfOrders', Ember.get(food,'numberOfOrders')-1);
+       food.save();
+      }
+     }
   }
 });
